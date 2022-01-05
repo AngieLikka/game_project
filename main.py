5f49c4ba20a67593f3ad39d2054b8dea71fb57bc
AngieLikka <a@be.pp.ru>
2022-01-05 23:27:39 +0300

    platforms and things made

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import sys
 from random import randint
 # from tiles_class import Tiles
-from things_class import Things
+# from things_class import Things
 from cat_class import Cat
 
 pygame.init()
@@ -43,6 +43,18 @@ class Tiles(pygame.sprite.Sprite):  # класс частей поля
         self.rect.x -= v // FPS
 
 
+class Things(pygame.sprite.Sprite):  # класс вещей, которые герой собирает
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
+        self.image = pygame.transform.scale(load_image('coin.jpg', -1), (30, 30))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        self.rect.x -= v // FPS
+
+
 def terminate():  # функция завершения игры
     pygame.quit()
     sys.exit()
@@ -53,14 +65,26 @@ def start_screen():  # функция запуска стартового экр
 
 
 def add_platform():
-    platform = Tiles(W, tile_y, tile_w)
-    tiles_group.add(platform)
+    tile_y = randint(0, H - 100)
+    tile_w = randint(300, 600)
+    a = 0
+    if check(tile_y):
+        platform = Tiles(W, tile_y, tile_w)
+        tiles_group.add(platform)
+        for i in range(tile_w // 60):
+            add_thing(W + a, tile_y)
+            a += 60
 
 
-def check(x, y, long):
+def add_thing(x, y):
+    thing = Things(x, y)
+    things_group.add(thing)
+
+
+def check(y):
     count = 0
     for i in tiles_group:
-        if i.rect.x != x and y not in range(i.rect.y - 50, i.rect.y + 50):
+        if y not in range(i.rect.y - 50, i.rect.y + 50):
             count += 1
     if len(tiles_group) == count:
         return True
@@ -82,23 +106,22 @@ while running:
     screen.blit(fon, (0, 0))
     pygame.display.set_caption('game')
     if len(tiles_group) < 100:
-        tile_y = randint(0, H - 100)
-        tile_w = randint(300, 500)
         if len(tiles_group) == 0:
             add_platform()
         else:
-            if check(W, tile_y, tile_w) is True:
-                add_platform()
-    for i in tiles_group:
-        if i.rect.x == 0:
-            # if check(W, tile_y, tile_w) is True:
             add_platform()
     for i in tiles_group:
+        if i.rect.x < 0:
+            add_platform()
         if i.rect.x + i.rect.width < 0:
+            i.kill()
+    for i in things_group:
+        if i.rect.x < 0:
             i.kill()
     tiles_group.draw(screen)
     tiles_group.update()
     things_group.draw(screen)
+    things_group.update()
     cat_group.draw(screen)
     pygame.display.flip()
     clock.tick(FPS)
--- things_class.py
+++ things_class.py
@@ -1,6 +1,13 @@
 import pygame
+from main import load_image, all_sprites, v, FPS
 
 
 class Things(pygame.sprite.Sprite):  # класс вещей, которые герой собирает
-    def __init__(self, *groups):
-        super().__init__(*groups)
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
+        self.image_coin = pygame.transform.scale(load_image('coin.jpg', -1), (100, 100))
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        self.rect.x -= v // FPS
