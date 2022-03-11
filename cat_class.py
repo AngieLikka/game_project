c00f1aba2b69c4cbd201b5478e3e938b89b27e95
aleshinave.23 <leriia-spb@yandex.ru>
2022-03-11 15:06:02 +0300

    Избавление от работы с гифками

--- cat_class.py
+++ cat_class.py
@@ -1,39 +1,34 @@
 import pygame
+from PIL import Image
 
 
 class Cat(pygame.sprite.Sprite):  # класс героя
     def __init__(self, im, help, coins, *groups):
         super().__init__(*groups)
         self.photo = im
-        self.num = 0
-        self.i = 0
+        self.num = 300
         self.transfer = help
         self.coins = coins
-        try:
-            while 1:
-                im.seek(self.num)
-                self.num += 1
-        except EOFError:
-            pass
-        im.seek(self.i)
-        im.save('new.png')
-        self.image = pygame.transform.scale(pygame.image.load('new.png'), (80, 55))
+        im = Image.open(self.photo)
+        im_crop = im.crop((0, 0, 300, 200))
+        im_crop.save('cat.png', quality=95)
+        self.image = pygame.transform.scale(pygame.image.load('cat.png'), (80, 55))
         self.rect = self.image.get_rect()
         self.rect.x = 200
         self.rect.y = 300
-        self.g = 1
         self.mask = pygame.mask.from_surface(self.image)
+
+    def new(self):
+        im = Image.open(self.photo)
+        self.num += 300
+        print(im.size)
+        self.num %= im.size[0]
+        im_crop = im.crop((self.num, 0, self.num + 300, 200))
+        im_crop.save('cat.png', quality=95)
+        self.image = pygame.transform.scale(pygame.image.load('cat.png'), (80, 55))
 
     def update(self, n, *args):
         global coin
-        if self.g == self.num * 5:
-            self.photo.seek(self.i)
-            self.photo.save('new.png')
-            self.i += 1
-            self.i %= self.num
-            self.image = pygame.transform.scale(pygame.image.load('new.png'), (80, 55))
-            self.g = 0
-        self.g += 1
         flag = True
         for i in self.transfer.get_tilesgroup():
             if pygame.sprite.collide_mask(self, i):
